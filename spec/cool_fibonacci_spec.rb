731c253d3888ab0ceb4a5261f81d9a35076a1323
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-04-21 16:31:55 -0700

    Clarify spec descriptions.

--- spec/cool_fibonacci_spec.rb
+++ spec/cool_fibonacci_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('cool_fibonacci')
 
 describe('Fixnum#cool_fibonacci') do
-  it("returns the returns the value of Fibonacci sequence corresponding to the integer it was called on") do
+  it("for the simplest case of 1, returns 1") do
     expect(1.cool_fibonacci()).to(eq(1))
   end
   it("returns the returns the value of Fibonacci sequence corresponding to the integer it was called on") do
